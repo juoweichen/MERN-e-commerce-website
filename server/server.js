4ead700283a99b5c1407bc430c3f7d02e7131e4a
pootitan <pootitan@gmail.com>
2020-08-30 22:46:54 +0800

    serve single react page from server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 console.log('Currenet node environment: ', process.env.NODE_ENV);
 
@@ -19,6 +20,17 @@ const port = process.env.PORT ?
 const server = app.listen(port, () => {
 	console.log('listening to port', port);
 })
+
+// Serve static files from the React frontend app
+if (process.env.NODE_ENV === 'production') {
+	const clinetPath = path.join(__dirname, '../client/build');
+	console.log('Serve client build at: ', clinetPath);
+	app.use(express.static(clinetPath))
+	// Anything that doesn't match the above, send back index.html
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname + '../client/build/index.html'))
+	})
+}
 
 // export server for testing
 module.exports = server;
