15cd0b6352d2f03d5775e8df4172babe047aa374
pootitan <pootitan@gmail.com>
2020-08-31 00:38:22 +0800

    fix client test

--- client/src/services/category.js
+++ client/src/services/category.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = `${process.env.REACT_APP_APIURL}/api/category/public`;
+const apiUrl = `/api/category/public`;
 
 export function getCategories() {
 	return axios.get(`${apiUrl}/all`);
