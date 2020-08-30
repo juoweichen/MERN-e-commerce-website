b2d1f6683c5a3da6eef860aa4a375f9860ea4b27
pootitan <pootitan@gmail.com>
2020-08-30 20:59:02 +0800

    server basic script

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+console.log('Currenet node environment: ', process.env.NODE_ENV);
+
+// middleware
+app.use(express.json());
+require('./src/init/cors')(app);
+
+// Listening to port
+const port = process.env.PORT ?
+	process.env.PORT :
+	(process.env.NODE_ENV === 'test' ? 5001 : 5000);
+const server = app.listen(port, () => {
+	console.log('listening to port', port);
+})
+
+// export server for testing
+module.exports = server;
