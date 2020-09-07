4004c90d0f7fff82661736d8ec7d042f9b2b5dce
pootitan <pootitan@gmail.com>
2020-09-08 00:20:33 +0800

    test print dburl

--- server/src/init/env.js
+++ server/src/init/env.js
@@ -4,6 +4,7 @@ module.exports = function () {
 	// TODO: do it different way
 	if (process.env.NODE_ENV === "test")
 		return;
+	console.log('*******test print DB_URL: ', process.env.DB_URL);
 	if (!process.env.DB_URL)
 		throw new Error("FATAL ERROR, env variable DB_URL is not defined");
 }
