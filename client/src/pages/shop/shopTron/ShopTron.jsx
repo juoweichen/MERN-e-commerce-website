5d9449bbba3eb0479e0d82f1ee6d890075d604dd
pootitan <pootitan@gmail.com>
2020-11-02 15:15:12 +0800

    v1.2.1: Add jumbotron image

--- client/src/pages/shop/shopTron/ShopTron.jsx
+++ client/src/pages/shop/shopTron/ShopTron.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Container, Jumbotron } from 'react-bootstrap';
 
+import './ShopTron.css';
+
 export default function ShopTron() {
 	return (
 		<Jumbotron>
 			<Container>
-				<h1>Clique Worldwide</h1>
-				<p>Some slogan</p>
+				<h1 id="title-tron">Clique Worldwide</h1>
+				<p id="slogan-tron">Clique Up, Be The Star of Your life</p>
 			</Container>
 		</Jumbotron>)
 }
